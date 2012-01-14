98ccd9ef648e729cad641993eacdde49ec07dd59
Tom SF Haines <thaines@gmail.com>
2012-01-14 22:18:20 +0000

    Fix for use of scipy kmeans in gmm - parameters were mapped incorrectly.

--- gmm/kmeans0.py
+++ gmm/kmeans0.py
@@ -23,35 +23,11 @@ from kmeans_shared import KMeansShared
 class KMeans0(KMeansShared):
   """Wraps the kmeans implimentation provided by scipy with the same interface as provided by the other kmeans implimentations in the system. My experiance shows that this is insanely slow - not sure why, but even my brute force C implimentation is faster (Best guess is its coded in python, and not optimised in any way.)."""
 
-  def doTrain(self, feats, clusters, maxIters = 512, restarts = 1, callback = None, assignOut = None):
+  def doTrain(self, feats, clusters, maxIters = 512, restarts = 8, assignOut = None):
     """Given a large number of features as a data matrix this finds a good set of cluster centres. clusters is the number of clusters to create, maxIters is the maximum number of iterations for convergance of a cluster and restarts how many times to run - the most probable result is used. minSize is the smallest cluster size - if a cluster is smaller than this it is reinitialised."""
+    
+    self.means = numpy.empty((clusters, feats.shape[1]), dtype=numpy.float_)
+    self.means[:,:] = scipy.cluster.vq.kmeans(feats, clusters, restarts)[0]
+    
+    if assignOut!=None: assignOut[:], _ = scipy.cluster.vq.vq(feats, centres)
 
-    if callback!=None:
-      callback(0,restarts+1)
-
-    # Required data structures...
-    bestModelScore = None # Score of the model currently in self.means
-    centres = numpy.empty((clusters, feats.shape[1]), dtype=numpy.float_)
-    assignment = numpy.empty(feats.shape[0], dtype=numpy.int_)
-
-    # Iterate and run the algorithm from scratch a number of times, calculating the log likelihood of the model each time, so we can ultimatly select the best...
-    for r in xrange(restarts):
-      if callback!=None:
-        callback(1+r,restarts+1)
-      
-      centres[:,:] = scipy.cluster.vq.kmeans(feats, clusters, maxIters)[0]
-      assignment[:], _ = scipy.cluster.vq.vq(feats, centres)
-
-
-      # Calculate the models negative log likelihood - if better than the current model replace it (This is done proportionally)...
-      if bestModelScore==None:
-        self.means = centres.copy()
-      else:
-        negLogLike = 0.0
-        for i in xrange(feats.shape[0]):
-          negLogLike += ((feats[i,:]-centres[assignment[i],:])**2).sum()
-
-        if negLogLike<bestModelScore:
-          self.means = centres.copy()
-
-      if assignOut!=None: assignOut[:] = assignment
