9066163cb3fa90c6c679af075a52dd9f846ebcdb
Tom SF Haines <thaines@gmail.com>
2013-12-16 18:51:24 +0000

    Work on multiplication capability.

--- ms/mult.c
+++ ms/mult.c
@@ -9,3 +9,46 @@
 
 
 #include "mult.h"
+
+
+
+float mult_area_mci(const Kernel * kernel, KernelConfig config, int dims, int terms, const float ** fv, const float ** scale, float * temp1, float * temp2, int samples, const unsigned int index[2])
+{
+ float norm = kernel->norm(dims, config);
+ 
+ float ret = 0.0;
+ 
+ unsigned int rng[3];
+ rng[0] = index[0];
+ rng[1] = index[1];
+ 
+ for (rng[2]=0; rng[2]<samples; rng[2]++)
+ {
+  // Draw from the first distribution, convert to global space...
+   kernel->draw(dims, config, rng, fv[0], temp1);
+   
+   int i;
+   for (i=0; i<dims; i++) temp1[i] /= scale[0][i];
+   
+  // Calculate its value, by multiplying with the probabilities of the remaining distributions - do everything in the global 'without scale' space for consistancy...
+   float prob = 1.0;
+   
+   int j;
+   for (j=1; j<terms; j++)
+   {
+    // Convert to local space and offset...
+     for (i=0; i<dims; i++) temp2[i] = temp1[i] * scale[j][i] - fv[j][i];
+     
+    // Multiply in the base normalised probability...
+     prob *= norm * kernel->weight(dims, config, temp2);
+     
+    // Factor in scaling effects...
+     for (i=0; i<dims; i++) prob *= scale[j][i];
+   }
+   
+  // Update ret with an incrimental average...
+   ret += (prob - ret) / (rng[2] + 1);
+ }
+  
+ return ret;
+}
--- ms/mult.h
+++ ms/mult.h
@@ -15,9 +15,17 @@
 
 
 
-// Uses importance sampling to approximate the area under the multiplication of several exemplars with associated kernel - effectivly the weight to use if you multiply these mixtures components together and put them into a mixture model with other ones...
-// Inputs are: kernel - the kernel applied to all feature vectors, its configuration and the result of calling norm for it; dims - the number of dimensions; terms is the number of terms in the multiplication; fv is an array of length terms pointing to an array of length dims - they are the feature centers fot the kernel to multiply together; scale is the scale parameters associated with each fv, for if they do not match; temp is a pointer to some temporary storage, of length dims; samples is how many importances samples to take, and index is the seed of the philox random number generator.
-float mult_area_is(const Kernel * kernel, KernelConfig config, float norm, int dims, int terms, const float ** fv, const float ** scale, float * temp, int samples, const unsigned int index[2]);
+// Uses monte carlo integration to approximate the area under the multiplication of several exemplars with associated kernel - effectivly the weight to use if you multiply these mixtures components together and put them into a mixture model with other ones...
+// Inputs are: kernel - the kernel applied to all feature vectors plus its configuration; dims - the number of dimensions; terms is the number of terms in the multiplication; fv is an array of length terms pointing to an array of length dims - they are the feature centers fot the kernel to multiply together; scale is the scale parameters associated with each fv, for if they do not match (It has been applied to those passed in - for conversion between spaces); temp1 and temp2 are both pointers to some temporary storage, of length dims; samples is how many importances samples to take, and index is the seed of the philox random number generator.
+// Return value is the area under the function created by multiplying the pdf's together.
+float mult_area_mci(const Kernel * kernel, KernelConfig config, int dims, int terms, const float ** fv, const float ** scale, float * temp1, float * temp2, int samples, const unsigned int index[2]);
+
+
+
+// Draws from the distribution implied by the multiplication of a bunch of distributions (kernels) - uses Metropolis-Hastings.
+// ************************
+float mult_draw_mh(const Kernel * kernel, KernelConfig config, int dims, int terms, const float ** fv, const float ** scale, float * out, float * temp, int steps, const unsigned int index[2]);
+
 
 
 #endif
