ff8f1a82d3da60c2df7d067931ac9089a7be4289
Tom SF Haines <thaines@gmail.com>
2014-05-06 08:38:30 +0100

    Addition of further modules to frf.

---
+++ frf/information.h
@@ -0,0 +1,35 @@
+#ifndef INFORMATION_H
+#define INFORMATION_H
+
+// Copyright 2014 Tom SF Haines
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+
+
+// Provides measures of the information of a set of exemplars, with add/remove capability and support for multiple output information measures...
+
+
+
+// Info type, that provides information on a single channel...
+typedef void * Info;
+
+
+
+// Function pointer types for the information object...
+
+// New, delete and reset...
+
+
+// Add and remove for exemplars in the data set...
+
+
+// Returns the amount of information within, noting that its the sum for all exemplars that have been added...
+
+
+
+#endif
---
+++ frf/tests.h
@@ -0,0 +1,18 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+// Copyright 2014 Tom SF Haines
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+
+
+// This provides tests, and code to optimise them based on information theory based measures of their value - its all fairly simple, though its designed to avoid allocating its own memory
+
+
+
+#endif
